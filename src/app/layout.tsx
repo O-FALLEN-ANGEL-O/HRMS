927fd599f1008bce73c67a6462232e709deac830
Prajwal Dl <dlprajwal183@gmail.com>
2025-07-20 16:49:09 +0000

    add dark mode to complete weapp also add progressive web app setting
     w

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { AuthProvider } from '@/hooks/use-auth';
 export const metadata: Metadata = {
   title: 'OptiTalent HRMS',
   description: 'A Next-Generation HRMS for modern businesses.',
+  manifest: '/manifest.json',
 };
 
 const inter = Inter({
