e5e9962544b9ce551d1062023f7d960f29484bc6
Prajwal Dl <dlprajwal183@gmail.com>
2025-07-22 13:40:40 +0000

    every page of every role sae issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -49,7 +49,7 @@ function AppLayout({ children }: { children: React.ReactNode }) {
         <SidebarProvider>
             <div className="flex h-screen bg-muted/40">
                 <AppSidebar />
-                <div className="flex-1 flex flex-col">
+                <div className="flex-1 flex flex-col overflow-hidden">
                     <AppHeader />
                     <main className="flex-1 overflow-y-auto">
                         <div className="p-4 sm:p-6 lg:p-8">
