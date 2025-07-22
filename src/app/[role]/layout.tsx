9ae2958edc348174a0d952fe85811be95c71346a
Prajwal Dl <dlprajwal183@gmail.com>
2025-07-22 09:29:53 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <AppContent>
    
    with

--- src/app/[role]/layout.tsx
+++ src/app/[role]/layout.tsx
@@ -18,7 +18,7 @@ function AppContent({ children }: { children: React.ReactNode }) {
         }
     }
     return (
-         <div className="flex-1 flex flex-col h-screen" onClick={handleContentClick}>
+         <div className="flex-1 flex flex-col" onClick={handleContentClick}>
             <AppHeader />
             <main className="flex-1 p-4 md:p-6 lg:p-8 overflow-y-auto">
                 {children}
