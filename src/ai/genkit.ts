b5216e203fbb8b8c3777deea6b674f7c108d0df6
Prajwal Dl <dlprajwal183@gmail.com>
2025-07-27 16:07:52 +0000

    chek why the ai is not working and fix tat without affecting the website

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -5,9 +5,7 @@ import { z } from 'zod';
 
 export const ai = genkit({
   plugins: [
-    googleAI({
-        apiKey: process.env.GEMINI_API_KEY,
-    }),
+    googleAI(),
     ],
   logLevel: 'debug',
   enableTracingAndMetrics: true,
